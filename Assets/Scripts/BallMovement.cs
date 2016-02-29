bccf739cdc03cdf0f8169a0a549064ad3b9d234d
tiagommferreira <tiagommferreira55@gmail.com>
2016-02-29 12:09:35 +0000

    ball script updated

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -3,31 +3,10 @@ using System.Collections;
 
 public class BallMovement : MonoBehaviour {
 
-	public GameObject ball;
-	Vector3 oldVel;
-
 	// Use this for initialization
 	void Start () {
-		Debug.Log ("ok");
-		//ball.AddForce(new Vector2(0,2));
-
-		Debug.Log ("ok");
-		ball = GameObject.Find("ball");
-		ball.GetComponent<Rigidbody2D>().AddForce(new Vector2(200,100)); 
+		GetComponent<Rigidbody2D>().AddForce(new Vector2(200,100)); 
 	}
 	
-	// Update is called once per frame
-	void FixedUpdate () {
-		oldVel = ball.GetComponent<Rigidbody2D> ().velocity;
-	}
-
-	void OnCollisionEnter (Collision c)
-	{
-		ContactPoint cp = c.contacts [0];
-
-		ball.GetComponent<Rigidbody2D> ().velocity = Vector3.Reflect (oldVel, cp.normal);
-
-		ball.GetComponent<Rigidbody2D> ().velocity = cp.normal * 2.0f;
-	}
 
 }
