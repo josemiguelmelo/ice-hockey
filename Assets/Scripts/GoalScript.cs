a8e5300a627692f694c4f4d25d242829d78f8b19
Jose Miguel Melo <melo199435@gmail.com>
2016-03-01 16:26:44 +0000

    check goal

---
+++ Assets/Scripts/GoalScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GoalScript : MonoBehaviour {
+	public Text goalText;
+
+	// Use this for initialization
+	void Start () {
+		goalText.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.name == "ball") 
+		{
+			goalText.enabled = true;
+		}
+
+	}
+}
