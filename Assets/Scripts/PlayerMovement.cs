fd31e3d3405c5e4a550b5bb1ef098baa97772e74
Ricardo Loureiro <ricardojrloureiro@gmail.com>
2016-03-04 14:01:20 +0000

    more flexible mouse events in player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,37 +14,46 @@ public class PlayerMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		Debug.Log ("melo foi criado");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetMouseButtonDown (0)) {
+			initialMousePosition = Input.mousePosition;
+			initTime = Time.time;
+		}
+
+		if(Input.GetMouseButtonUp(0)) {
+
+			float endTime = Time.time;
+			float timeTaken = endTime - initTime;
+
+			Vector3 endPosition = Input.mousePosition;
+			float distance = Vector3.Distance (initialMousePosition, endPosition);
+
+			Vector3 differenceVector = ( endPosition - initialMousePosition).normalized;
+
+			GameObject player = GameObject.Find ("player_1");
+			Rigidbody2D playerBody = player.GetComponent<Rigidbody2D> ();
+
+			movementSpeed = distance / timeTaken;
+
+			playerBody.AddForce(
+				new Vector2( movementSpeed * differenceVector.x , 
+					movementSpeed * differenceVector.y )
+			); 
+		}
 	}
 
 	void OnMouseDown()
 	{
-		initialMousePosition = Input.mousePosition;
-		initTime = Time.time;
+		Debug.Log ("melo clickou");
 	}
 
 	void OnMouseUp()
 	{
-		float endTime = Time.time;
-		float timeTaken = endTime - initTime;
-
-
-		Vector3 endPosition = Input.mousePosition;
-		float distance = Vector3.Distance (initialMousePosition, endPosition);
-
-		Vector3 differenceVector = ( endPosition - initialMousePosition).normalized;
-
-		movementSpeed = distance / timeTaken;
-
-		GetComponent<Rigidbody2D>().AddForce(
-			new Vector2( movementSpeed * differenceVector.x , 
-				movementSpeed * differenceVector.y )
-		); 
+		Debug.Log ("melo largou");
 	}
 
 }
